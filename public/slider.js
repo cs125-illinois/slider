827c8b9d7d02de2fef73c4b4d99cc153a8cfe5b9
Geoffrey Challen <challen@illinois.edu>
2018-01-21 17:08:27 -0600

    Working.

--- public/slider.js
+++ public/slider.js
@@ -1,17 +1,19 @@
-var socket = socketCluster.connect()
-socket.on('error', function (err) {
-  throw 'Socket error - ' + err
-})
+const clientID = '948918026196-p399ooibc7pr0ci7ida63jb5a6n4vsik.apps.googleusercontent.com'
+let socket = socketCluster.connect()
+socket.on('error', function (err) { throw(err) })
+
+let login = (token) => {
+  socket.emit('login', token)
+}
+window.googleLoginHelper
+  .config(clientID)
+  .login(token => {
+    login(token)
+  })
+  .manual(error => {
+    // Need to enable manual signin here
+    console.log(error)
+  })
 socket.on('connect', function () {
   window.googleLoginHelper.start()
 })
-
-window.googleLoginHelper
-  .config('531745357390-akh3vsslmb6c3jmufjrqqr5qkif95g1i.apps.googleusercontent.com')
-  .login(user => {
-    $("#signin").hide()
-  })
-  .manual(error => {
-    $("#signin").show()
-  })
-
