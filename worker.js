b81ca88f77c09a8ab81d68c6fdd6ad5449357cec
Geoffrey Challen <challen@illinois.edu>
2018-01-22 02:09:29 -0600

    Simple stuff working for today.

--- worker.js
+++ worker.js
@@ -6,39 +6,43 @@ const clientID = '948918026196-p399ooibc7pr0ci7ida63jb5a6n4vsik.apps.googleuserc
 const { OAuth2Client } = require('google-auth-library')
 var client = new OAuth2Client(clientID, '', '');
 
+let count = 0
 class Slider extends SocketWorker {
-  async login (token, respond, socket) {
-    console.log('login')
+  async login (info, respond, socket) {
     try {
       var login = await client.verifyIdToken({
-        idToken: token,
+        idToken: info.token,
         audience: clientID
       })
     } catch (err) {
-      console.log(err)
       return respond('Login failed')
     }
     try {
       let payload = login.getPayload()
-      console.log(payload)
       expect(payload.hd).to.equal('illinois.edu')
       socket.setAuthToken({
-        username: payload.email
+        email: payload.email
       })
-      console.log('done')
-      respond()
+      return respond()
     } catch (err) {
-      console.log(err)
-      respond('Please log in with your @illinois.edu email address')
+      return respond('Please log in with your @illinois.edu email address')
     }
+  }
+  report (change) {
   }
   run () {
     this.scServer.on('connection', (socket) => {
       socket.on('login', (token, respond) => {
         return this.login(token, respond, socket)
       })
-      socket.on('disconnect', () => {
-        console.log('disconnect')
+      socket.on('slideChange', (currentSlide, respond) => {
+        let authToken = socket.getAuthToken()
+        if (!(authToken)) {
+          return respond('authentication required')
+        }
+        currentSlide.email = authToken.email
+        console.log(currentSlide)
+        return respond()
       })
     })
   }
