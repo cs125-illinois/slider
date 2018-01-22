bfd642d27f25459877673714685f4a22a1326fe0
Geoffrey Challen <challen@illinois.edu>
2018-01-21 23:06:40 -0600

    Working.

--- public/slider.js
+++ public/slider.js
@@ -2,18 +2,27 @@ const clientID = '948918026196-p399ooibc7pr0ci7ida63jb5a6n4vsik.apps.googleuserc
 let socket = socketCluster.connect()
 socket.on('error', function (err) { throw(err) })
 
-let login = (token) => {
-  socket.emit('login', token)
+let login = (user) => {
+  socket.emit('login', user, function (err) {
+    console.log(err)
+    if (err) {
+      window.googleLoginHelper.start()
+    }
+  })
 }
 window.googleLoginHelper
   .config(clientID)
-  .login(token => {
-    login(token)
+  .login(user => {
+    login(user)
   })
   .manual(error => {
     // Need to enable manual signin here
     console.log(error)
   })
-socket.on('connect', function () {
-  window.googleLoginHelper.start()
+socket.on('connect', function (status) {
+  if (!(status.isAuthenticated)) {
+    window.googleLoginHelper.start()
+  } else {
+    console.log("Already authenticated")
+  }
 })
--- worker.js
+++ worker.js
@@ -1,22 +1,42 @@
 const SocketWorker = require('socketcluster/scworker')
 
-const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const expect = require('chai').expect
+
+const clientID = '948918026196-p399ooibc7pr0ci7ida63jb5a6n4vsik.apps.googleusercontent.com'
+const { OAuth2Client } = require('google-auth-library')
+var client = new OAuth2Client(clientID, '', '');
 
 class Slider extends SocketWorker {
-  login(token) {
-    console.log(token)
+  async login (token, respond, socket) {
+    console.log('login')
+    try {
+      var login = await client.verifyIdToken({
+        idToken: token,
+        audience: clientID
+      })
+    } catch (err) {
+      console.log(err)
+      return respond('Login failed')
+    }
+    try {
+      let payload = login.getPayload()
+      console.log(payload)
+      expect(payload.hd).to.equal('illinois.edu')
+      socket.setAuthToken({
+        username: payload.email
+      })
+      console.log('done')
+      respond()
+    } catch (err) {
+      console.log(err)
+      respond('Please log in with your @illinois.edu email address')
+    }
   }
-  run() {
-    var app = express()
-    app.use(serveStatic(path.resolve(__dirname, 'public')))
-    this.httpServer.on('request', app)
-
+  run () {
     this.scServer.on('connection', (socket) => {
-
-      socket.on('login', this.login)
-
+      socket.on('login', (token, respond) => {
+        return this.login(token, respond, socket)
+      })
       socket.on('disconnect', () => {
         console.log('disconnect')
       })
